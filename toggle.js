9a82a61c963dc92b5b52d2ab2dca6f1da545e75f
babikirm <mbabikir2@gmail.com>
2022-12-09 00:27:01 -0600

    New Toggle

--- toggle.js
+++ toggle.js
@@ -1,4 +1,4 @@
-function toggleFont() {
+/* function toggleFont() {
     var body = document.getElementsByTagName("body")[0];
     var font = body.style.fontFamily;
     if (font == "dyslexia") {
@@ -6,4 +6,21 @@ function toggleFont() {
     } else {
     body.style.fontFamily = "dyslexia";
     }
-}
+} */
+
+const isPressed = window.localStorage.getItem('dyslexic') === 'true';
+    if(isPressed) {
+        document.body.classList.add('dyslexia-mode');
+    }
+    // set the button to pressed if appropriate
+    const toggle = document.getElementById('dyslexia-toggle');
+    if(isPressed) {
+        toggle.setAttribute('aria-pressed', 'true');
+    }
+    // toggle dyslexia support
+    toggle.addEventListener('click', (e) => {
+        let pressed = e.target.getAttribute('aria-pressed') === 'true';
+        e.target.setAttribute('aria-pressed', String(!pressed));
+        document.body.classList.toggle('dyslexia-mode');
+        window.localStorage.setItem('dyslexic', String(!pressed));
+    });
